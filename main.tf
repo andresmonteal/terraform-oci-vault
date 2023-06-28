cb8171b38c7c29f478773a4fa1ea78249ae6f389
Andres Montealegre <andres.montealegre@oracle.com>
2023-06-28 09:12:02 -0500

    new feature: adds master key

--- main.tf
+++ main.tf
@@ -29,3 +29,26 @@ resource "oci_kms_vault" "main" {
     delete = "20m"
   }
 }
+
+resource "oci_kms_key" "test_key" {
+  #Required
+  compartment_id = local.compartment_id
+  display_name   = var.key_display_name
+  key_shape {
+    #Required
+    algorithm = var.key_shape_algorithm
+    length    = var.key_shape_length
+  }
+  management_endpoint = var.key_management_endpoint
+
+  #Optional
+  defined_tags    = var.defined_tags
+  freeform_tags   = local.merged_freeform_tags
+  protection_mode = var.key_protection_mode
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -40,3 +40,35 @@ variable "defined_tags" {
   type        = map(any)
   default     = null
 }
+
+# vault key
+
+variable "key_display_name" {
+  description = "A user-friendly name for the key. It does not have to be unique, and it is changeable. Avoid entering confidential information."
+  type        = string
+  default     = "master-key"
+}
+
+variable "key_shape_algorithm" {
+  description = "The algorithm used by a key's key versions to encrypt or decrypt."
+  type        = string
+  default     = "AES"
+}
+
+variable "key_shape_length" {
+  description = "The length of the key in bytes, expressed as an integer."
+  type        = string
+  default     = "32"
+}
+
+variable "key_management_endpoint" {
+  description = "The length of the key in bytes, expressed as an integer."
+  type        = string
+  default     = "https://kms.us-ashburn-1.oraclecloud.com"
+}
+
+variable "key_protection_mode" {
+  description = "Required) The service endpoint to perform management operations against. Management operations include 'Create,' 'Update,' 'List,' 'Get,' and 'Delete' operations. See Vault Management endpoint."
+  type        = string
+  default     = "HSM"
+}
